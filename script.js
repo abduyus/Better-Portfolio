61dabc90aa999f7d9427f4e042bdbcec028c2514
abduyus <35887528+abduyus@users.noreply.github.com>
2023-11-27 19:09:41 +0000

    fixing the link problem

--- script.js
+++ script.js
@@ -14,20 +14,18 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
     const href = link.getAttribute("href");
 
     // Scroll back to top
     if (href === "#") {
+      e.preventDefault();
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
-    }
-
-    if (href !== "#" && href.startsWith("#")) {
+    } else if (href.startsWith("#")) {
+      e.preventDefault();
       const sectionEl = document.querySelector(href);
-      console.log(sectionEl);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
   });
